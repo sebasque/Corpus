efa034feb8c3fd5757568d067b5da12b51c9d585
Michael Koohang <sebamin@proton.me>
2025-10-06 23:18:35 -0400

    Initial Commit

---
+++ CorpusTests/CorpusTests.swift
@@ -0,0 +1,17 @@
+//
+//  CorpusTests.swift
+//  CorpusTests
+//
+//  Created by Michael Koohang on 10/6/25.
+//
+
+import Testing
+@testable import Corpus
+
+struct CorpusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
